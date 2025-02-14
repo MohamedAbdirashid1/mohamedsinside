33a6cc9f48c706867a3bc5f5f002af1c45419d62
MohamedAbdirashid1 <Mohamedkheyr07@Outlook.com>
2025-02-14 14:42:16 +0000

    forsøkte å fikse feil i authenticate.php (registrering funker ikke også)

--- authenticate.php
+++ authenticate.php
@@ -10,19 +10,18 @@ $DATABASE_NAME = 'login_db';  // legg inn navnet på databasen din
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, 
 $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
-	// If there is an error with the connection, stop the script and display the 
-	error.
+	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
-
+var_dump($_POST);
 // Now we check if the data from the login form (index.hmtl-filen) was submitted, isset() will check if the data exists. !isset = data don't exist. ! = not.
 if ( !isset($_POST['username'], $_POST['password']) ) {
 	// Could not get the data that should have been sent.
 	exit('Please fill both the username and password fields!');
 }
 
-if ($stmt = $con->prepare('SELECT bruker_id, passord FROM ditt_tabellnavn WHERE 
-brukernavn = ?')) {
+if ($stmt = $con->prepare('SELECT id, password FROM users WHERE 
+username = ?')) {
 	// Bind parameters (s = string, i = int,), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
@@ -30,12 +29,12 @@ brukernavn = ?')) {
 	$stmt->store_result();
 
   if ($stmt->num_rows > 0) {
-    $stmt->bind_result($bruker_id, $passord);
+    $stmt->bind_result($bruker_id, $password);
     $stmt->fetch();
     // Account exists, now we verify the password.
     // Note: remember to use password_hash in your registration file to store the hashed passwords.
 // Linjen under må se slik ut!!
-if (password_verify($_POST['password'], $passord)) {
+if (password_verify($_POST['password'], $password)) {
       // Verification success! User has logged-in!
       // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
       session_regenerate_id();
